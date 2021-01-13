fd5f85fb7b485918a2256857d3ebfc86e01b084c
Potapov Valery <Potapovvp90@gmail.com>
2021-01-13 22:51:16 +0300

    Добавил модели БД в основной проект

--- LapshaApp/LapshaApp.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ LapshaApp/LapshaApp.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"c710cc78-4f82-46c4-b194-4108ac4c2e6f"
+	.asciz	"83537eb6-eed6-466a-916a-b9c88bd3cd3b"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
