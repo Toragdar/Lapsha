f83764720ec50529f1dfb7ec164654fa007c6a75
Potapov Valery <Potapovvp90@gmail.com>
2021-03-28 23:37:54 +0300

    -

--- LapshaApp/LapshaApp/Views/MainScreen.xaml.cs
+++ LapshaApp/LapshaApp/Views/MainScreen.xaml.cs
@@ -23,60 +23,177 @@ namespace LapshaApp
         {
             return new ObservableCollection<TestDay>
             {
-                new TestDay{DayName = "Monday", TestProducts = new ObservableCollection<TestProduct>
+                new TestDay
+                {
+                    DayName = "Monday", TestMeals = new ObservableCollection<TestMeal>
+                    { 
+                        new TestMeal 
+                        {
+                            MealNum = 1, TestProducts = new ObservableCollection<TestProduct>
+                            {
+                                new TestProduct {ProductName = "Potato", ProductWeight = 100},
+                                new TestProduct {ProductName = "Tomato", ProductWeight = 150}
+                            }
+                        },
+                        new TestMeal
+                        {
+                            MealNum = 2, TestProducts = new ObservableCollection<TestProduct>
+                            {
+                                new TestProduct {ProductName = "Egg", ProductWeight = 55},
+                                new TestProduct {ProductName = "Salad", ProductWeight = 300}
+                            }
+                        }
+                    } 
+                },
+                new TestDay
+                {
+                    DayName = "Tuesday", TestMeals = new ObservableCollection<TestMeal>
                     {
-                        new TestProduct {ProductName = "Potato", ProductWeight = 100},
-                        new TestProduct {ProductName = "Tomato", ProductWeight = 150}
+                        new TestMeal
+                        {
+                            MealNum = 1, TestProducts = new ObservableCollection<TestProduct>
+                            {
+                                new TestProduct {ProductName = "Potato", ProductWeight = 100},
+                                new TestProduct {ProductName = "Tomato", ProductWeight = 150}
+                            }
+                        },
+                        new TestMeal
+                        {
+                            MealNum = 2, TestProducts = new ObservableCollection<TestProduct>
+                            {
+                                new TestProduct {ProductName = "Egg", ProductWeight = 55},
+                                new TestProduct {ProductName = "Salad", ProductWeight = 300}
+                            }
+                        }
                     }
                 },
-                new TestDay{DayName = "Tuesday", TestProducts = new ObservableCollection<TestProduct>
+                new TestDay
+                {
+                    DayName = "Wednesday", TestMeals = new ObservableCollection<TestMeal>
                     {
-                        new TestProduct {ProductName = "Egg", ProductWeight = 50},
-                        new TestProduct {ProductName = "Salad", ProductWeight = 200}
+                        new TestMeal
+                        {
+                            MealNum = 1, TestProducts = new ObservableCollection<TestProduct>
+                            {
+                                new TestProduct {ProductName = "Potato", ProductWeight = 100},
+                                new TestProduct {ProductName = "Tomato", ProductWeight = 150}
+                            }
+                        },
+                        new TestMeal
+                        {
+                            MealNum = 2, TestProducts = new ObservableCollection<TestProduct>
+                            {
+                                new TestProduct {ProductName = "Egg", ProductWeight = 55},
+                                new TestProduct {ProductName = "Salad", ProductWeight = 300}
+                            }
+                        }
                     }
                 },
-                new TestDay{DayName = "Wednesday", TestProducts = new ObservableCollection<TestProduct>
+                new TestDay
+                {
+                    DayName = "Thursday", TestMeals = new ObservableCollection<TestMeal>
                     {
-                        new TestProduct {ProductName = "Egg", ProductWeight = 50},
-                        new TestProduct {ProductName = "Salad", ProductWeight = 200}
+                        new TestMeal
+                        {
+                            MealNum = 1, TestProducts = new ObservableCollection<TestProduct>
+                            {
+                                new TestProduct {ProductName = "Potato", ProductWeight = 100},
+                                new TestProduct {ProductName = "Tomato", ProductWeight = 150}
+                            }
+                        },
+                        new TestMeal
+                        {
+                            MealNum = 2, TestProducts = new ObservableCollection<TestProduct>
+                            {
+                                new TestProduct {ProductName = "Egg", ProductWeight = 55},
+                                new TestProduct {ProductName = "Salad", ProductWeight = 300}
+                            }
+                        }
                     }
                 },
-                new TestDay{DayName = "Thursday", TestProducts = new ObservableCollection<TestProduct>
+                new TestDay
+                {
+                    DayName = "Friday", TestMeals = new ObservableCollection<TestMeal>
                     {
-                        new TestProduct {ProductName = "Egg", ProductWeight = 50},
-                        new TestProduct {ProductName = "Salad", ProductWeight = 200}
+                        new TestMeal
+                        {
+                            MealNum = 1, TestProducts = new ObservableCollection<TestProduct>
+                            {
+                                new TestProduct {ProductName = "Potato", ProductWeight = 100},
+                                new TestProduct {ProductName = "Tomato", ProductWeight = 150}
+                            }
+                        },
+                        new TestMeal
+                        {
+                            MealNum = 2, TestProducts = new ObservableCollection<TestProduct>
+                            {
+                                new TestProduct {ProductName = "Egg", ProductWeight = 55},
+                                new TestProduct {ProductName = "Salad", ProductWeight = 300}
+                            }
+                        }
                     }
                 },
-                new TestDay{DayName = "Friday", TestProducts = new ObservableCollection<TestProduct>
+                new TestDay
+                {
+                    DayName = "Saturday", TestMeals = new ObservableCollection<TestMeal>
                     {
-                        new TestProduct {ProductName = "Egg", ProductWeight = 50},
-                        new TestProduct {ProductName = "Salad", ProductWeight = 200}
+                        new TestMeal
+                        {
+                            MealNum = 1, TestProducts = new ObservableCollection<TestProduct>
+                            {
+                                new TestProduct {ProductName = "Potato", ProductWeight = 100},
+                                new TestProduct {ProductName = "Tomato", ProductWeight = 150}
+                            }
+                        },
+                        new TestMeal
+                        {
+                            MealNum = 2, TestProducts = new ObservableCollection<TestProduct>
+                            {
+                                new TestProduct {ProductName = "Egg", ProductWeight = 55},
+                                new TestProduct {ProductName = "Salad", ProductWeight = 300}
+                            }
+                        }
                     }
                 },
-                new TestDay{DayName = "Saturday", TestProducts = new ObservableCollection<TestProduct>
+                new TestDay
+                {
+                    DayName = "Sunday", TestMeals = new ObservableCollection<TestMeal>
                     {
-                        new TestProduct {ProductName = "Egg", ProductWeight = 50},
-                        new TestProduct {ProductName = "Salad", ProductWeight = 200}
+                        new TestMeal
+                        {
+                            MealNum = 1, TestProducts = new ObservableCollection<TestProduct>
+                            {
+                                new TestProduct {ProductName = "Potato", ProductWeight = 100},
+                                new TestProduct {ProductName = "Tomato", ProductWeight = 150}
+                            }
+                        },
+                        new TestMeal
+                        {
+                            MealNum = 2, TestProducts = new ObservableCollection<TestProduct>
+                            {
+                                new TestProduct {ProductName = "Egg", ProductWeight = 55},
+                                new TestProduct {ProductName = "Salad", ProductWeight = 300}
+                            }
+                        }
                     }
                 },
-                new TestDay{DayName = "Sunday", TestProducts = new ObservableCollection<TestProduct>
-                    {
-                        new TestProduct {ProductName = "Egg", ProductWeight = 50},
-                        new TestProduct {ProductName = "Salad", ProductWeight = 200}
-                    }
-                }
             };
         }
         public class TestDay
         {
             public string DayName { get; set; }
-            public ObservableCollection<TestProduct> TestProducts { get; set; }
+            public ObservableCollection<TestMeal> TestMeals { get; set; }
 
         }
         public class TestProduct
         {
             public string ProductName { get; set; }
             public int ProductWeight { get; set; }
+        }
+        public class TestMeal
+        {
+            public int MealNum { get; set; }
+            public ObservableCollection<TestProduct> TestProducts { get; set; }
         }
     }
 }
