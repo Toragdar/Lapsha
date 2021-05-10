4b85b843070de1e5e0b34407b3cba7608faa515d
Potapov Valery <Potapovvp90@gmail.com>
2021-05-10 23:18:30 +0300

    Add searchLine

--- LapshaApp/LapshaApp.Android/obj/Debug/100/android/environment.armeabi-v7a.s
+++ LapshaApp/LapshaApp.Android/obj/Debug/100/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"55d2a03c-7c44-435a-95cf-beb4b4ad9eff"
+	.asciz	"16ed6975-cdaf-4c0b-9298-034a6340b250"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
---
+++ LapshaApp/LapshaApp/Views/SearchScreen.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LapshaApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchScreen : ContentPage
+    {
+        public SearchScreen()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+        private async void ClearEntryField(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
