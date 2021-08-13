aeb23633ff352d18290207b15bee1785c0be13e8
Potapov Valery <potapovvp90@gmail.com>
2021-08-13 16:17:34 +0300

    -

--- LapshaApp/LapshaApp.Android/obj/Debug/100/android/environment.armeabi-v7a.s
+++ LapshaApp/LapshaApp.Android/obj/Debug/100/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"35139ce6-3375-4a20-89db-6b5c8851b2ba"
+	.asciz	"e73c227a-30e4-4b99-9c16-7532aa42eb08"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
